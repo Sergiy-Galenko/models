ff0958f5525456c37d194f2759066e3251457ffe
kavliukigor <kavliukigor@gmail.com>
2026-05-13 23:01:10 +0300

    feat: remove UserSerializer and add RegisterView and UserProfileView for user registration and profile management

--- django/config/users/views.py
+++ django/config/users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import User
+from .serializers import UserSerializer, RegisterSerializer
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset=User.Objects.all()
+    serializer_class=RegisterSerializer
+    permision_classes=[permissions.AllowAny]
+
+class UserProfileView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class=UserSerializer
+    permission_classes=[permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
