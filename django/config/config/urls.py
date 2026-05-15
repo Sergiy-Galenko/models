c22b3e132114019e44d99ffea2b4ead832662114
kavliukigor <kavliukigor@gmail.com>
2026-05-15 10:34:59 +0300

    feat: integrate JWT authentication and update URL routing for token management

--- django/config/config/urls.py
+++ django/config/config/urls.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenBlacklistView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,4 +12,7 @@ urlpatterns = [
     path('internal/catalog/', include('catalog.urls')),
     path('internal/content/', include('content.urls')),
     path('internal/forms/', include('forms.urls')),
+    path('internal/auth/token/', TokenObtainPairView.as_view(), name='token_obtain'),
+    path('internal/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('internal/auth/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
 ]
